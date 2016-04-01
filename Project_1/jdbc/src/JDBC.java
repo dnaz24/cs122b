eaa1113581021007ab68d64bb8527abf6bb805e5
Colin Son <ccson@uci.edu>
2016-04-01 14:10:43 -0700

    Fixed up Problem 2 Part 1 so that it can be in one function, instead of 2.

--- Project_1/jdbc/src/JDBC.java
+++ Project_1/jdbc/src/JDBC.java
@@ -1,34 +1,40 @@
 package jdbc; 
 
 import java.sql.*;
+import java.util.*;
 
 public class JDBC
 {
-
-	public static void print(Connection connection, String FirstTerm) throws Exception{
-
-		Statement select = connection.createStatement();
-		ResultSet result = select.executeQuery("SELECT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \"NULL\", m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \"NULL\", m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND (s.first_name = \"" + FirstTerm + "\" OR s.last_name = \"" + FirstTerm + "\" OR s.id = \"" + FirstTerm + "\")");
-
-		while(result.next()){
-			
-			System.out.println("Movie ID: " + result.getInt(1));
-			System.out.println("Movie Title: " + result.getString(2));
-			System.out.println("Movie Year: " + result.getInt(3));
-			System.out.println("Movie Director: " + result.getString(4)); 
-			System.out.println("Banner URL: " + result.getString(5));
-			System.out.println("Trailer URL: " + result.getString(6));
-			System.out.println();
-			
-		}
+	
+	private static ArrayList<String> parseQuery(String Query){
+		
+		ArrayList<String> parsedQuery = new ArrayList<String>(); 
+		
+		Scanner in = new Scanner(Query);
+		in.useDelimiter(" "); 
+		
+		while(in.hasNext())
+			parsedQuery.add(in.next()); 
+		
+		in.close(); 
+		return parsedQuery; 
 		
 	}
-	
-	public static void print(Connection connection, String FirstTerm, String SecondTerm) throws Exception{
 
+	public static void print(Connection connection, String Query) throws Exception{
+
+		ArrayList<String> parsedQuery = parseQuery(Query); 
+		
 		Statement select = connection.createStatement();
-		ResultSet result = select.executeQuery("SELECT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \"NULL\", m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \"NULL\", m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND ((s.first_name = \"1"  + FirstTerm + "\"AND s.last_name = \"" + SecondTerm + "\") OR (s.first_name =  \"" + FirstTerm + "\" OR s.last_name = \"" + SecondTerm + "\"))");
-
+		ResultSet result; 
+		
+		if (parsedQuery.size() == 1)
+			result = select.executeQuery("SELECT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \"NULL\", m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \"NULL\", m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND (s.first_name = \"" + parsedQuery.get(0) + "\" OR s.last_name = \"" + parsedQuery.get(0) + "\" OR s.id = \"" + parsedQuery.get(0) + "\")");
+		else if (parsedQuery.size() == 2)
+			result = select.executeQuery("SELECT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \"NULL\", m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \"NULL\", m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND ((s.first_name = \"1"  + parsedQuery.get(0) + "\"AND s.last_name = \"" + parsedQuery.get(1) + "\") OR (s.first_name =  \"" + parsedQuery.get(0) + "\" OR s.last_name = \"" + parsedQuery.get(1) + "\"))");
+		else
+			return; 
+		
 		while(result.next()){
 			
 			System.out.println("Movie ID: " + result.getInt(1));
@@ -77,7 +83,7 @@ public class JDBC
 
 		Connection connection = DriverManager.getConnection("jdbc:mysql:///moviedb", "root", "calmdude6994");
 		
-		print(connection, "james", "caan"); 
+		print(connection, "james caan"); 
 
 	}
 }
